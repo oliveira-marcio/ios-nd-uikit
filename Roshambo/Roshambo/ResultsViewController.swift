d7771801d538652dd794a58a8429f1cf654870d1
oliveira-marcio <emaildomso@gmail.com>
2020-08-03 20:46:16 +0100

    feat: Add results view controller

---
+++ Roshambo/Roshambo/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  Roshambo
+//
+//  Created by Márcio Oliveira on 8/3/20.
+//  Copyright © 2020 Márcio Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func dismiss() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}

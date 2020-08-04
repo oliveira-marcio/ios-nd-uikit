556567ff363796a0ff9dbcb8c3c73dfe3cb44563
oliveira-marcio <emaildomso@gmail.com>
2020-08-04 09:33:24 +0100

    feat: Compute and display results

--- Roshambo/Roshambo/ResultsViewController.swift
+++ Roshambo/Roshambo/ResultsViewController.swift
@@ -17,6 +17,10 @@ class ResultsViewController: UIViewController {
     var humanValue: Int?
     var computerValue: Int?
     
+    enum Moves: Int {
+        case rock = 0, paper, scissors
+    }
+    
      // MARK: Outlets
     
     @IBOutlet var resultLabel: UILabel!
@@ -24,16 +28,44 @@ class ResultsViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        computeResults()
+    }
+     
+    // MARK: Compute results
+    
+    func computeResults() {
         if let humanValue = self.humanValue {
             if let computerValue = self.computerValue {
-                self.resultLabel.text = "Player: \(humanValue) - \(computerValue)"
+                let humanMove = Moves(rawValue: humanValue)!
+                let computerMove = Moves(rawValue: computerValue)!
+                
+                if (humanMove == .rock && computerMove == .scissors) || (humanMove == .scissors && computerMove == .rock) {
+                    displayWinner(humanWinner: humanMove == .rock, message: "Rock crushes scissors!", image: "RockCrushesScissors")
+                } else if (humanMove == .paper && computerMove == .rock) || (humanMove == .rock && computerMove == .paper) {
+                    displayWinner(humanWinner: humanMove == .paper, message: "Paper covers rock!", image: "PaperCoversRock")
+                } else if (humanMove == .scissors && computerMove == .paper) || (humanMove == .paper && computerMove == .scissors) {
+                    displayWinner(humanWinner: humanMove == .scissors, message: "Scissors cut paper!",image:  "ScissorsCutPaper")
+                } else {
+                    displayWinner(humanWinner: nil, message: "It's a Tie!", image: "itsATie")
+                }
             } else {
                 showAlert("Oops", message: "Error getting computer value")
             }
         } else {
             showAlert("Oops", message: "Error getting human value")
         }
+    }
+    
+    // MARK: Display results
+    
+    func displayWinner(humanWinner: Bool?, message: String, image: String) {
+        var messageWinner = ""
+        if let humanWinner = humanWinner {
+            messageWinner = " You \(humanWinner ? "win" : "loose")."
+        }
+        
+        self.resultLabel.text = "\(message)\(messageWinner)"
+        self.resultImage.image = UIImage(named: image)
     }
     
      // MARK: Error handling

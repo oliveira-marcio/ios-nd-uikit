ca01433c91d4388d350016c75330d6e5fb5f0369
oliveira-marcio <emaildomso@gmail.com>
2020-08-05 20:26:01 +0100

    feat: Finish cash text field delegate

--- TextFieldsChallenge/TextFieldsChallenge/Text Fields Delegates/CashTextFieldDelegate.swift
+++ TextFieldsChallenge/TextFieldsChallenge/Text Fields Delegates/CashTextFieldDelegate.swift
@@ -11,7 +11,22 @@ import UIKit
 
 class CashTextFieldDelegate : NSObject, UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        print("cash: \(string)")
-        return true
+        if isNumericOrBackspace(string) {
+            var newText = textField.text! as NSString
+            newText = newText.replacingCharacters(in: range, with: string) as NSString
+            newText = newText.replacingOccurrences(of: ".", with: "") as NSString
+            newText = newText.replacingOccurrences(of: "$", with: "") as NSString
+
+            textField.text = String(format: "$%.02f", (Double(newText as String) ?? 0) / 100)
+        }
+        return false
+    }
+    
+    func isNumericOrBackspace(_ string: String) -> Bool {
+        return Int(string) != nil || strcmp(string, "\\b") == -92
+    }
+    
+    func isBackSpace(_ string: String) -> Bool {
+        return strcmp(string, "\\b") == -92
     }
 }

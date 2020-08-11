3d94d1c16468fdd680f17695bfbd45684d25d4f9
oliveira-marcio <emaildomso@gmail.com>
2020-08-11 12:21:44 +0100

    feat: step 4.7 - Finished

--- step4.7-roshamboWithHistory-incomplete/RockPaperScissors/HistoryViewController.swift
+++ step4.7-roshamboWithHistory-incomplete/RockPaperScissors/HistoryViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 // MARK: - HistoryViewController: UIViewController
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
     
     // MARK: Properties
     
@@ -21,8 +22,39 @@ class HistoryViewController: UIViewController {
         print(self.history)
     }
     
+    // MARK: Actions
+    
     @IBAction func backButtonPressed(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
-
+    
+    // MARK: Table View Delegate
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.history.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath)
+        let match = self.history[indexPath.row]
+        
+        cell.textLabel?.text = resultDefaultLabel(match)
+        cell.detailTextLabel?.text = resultDetailLabel(match)
+        
+        return cell
+    }
+    
+    // MARK: Results for cells labels
+    
+    func resultDefaultLabel(_ match: RPSMatch) -> String {
+        if match.p1 == match.p2 {
+            return "Tie!"
+        }
+        
+        return match.p1.defeats(match.p2) ? "Win!" : "Lose!"
+    }
+    
+    func resultDetailLabel(_ match: RPSMatch) -> String {
+        return match.p1.description + " vs " + match.p2.description
+    }
 }
